81a6931b77fa46fe559c40c5b7cc051f354e4f36
Camille Berger <Camille@Camilles-MacBook-Pro.local>
2016-06-09 12:21:08 -0400

    GuessPhrase with comments

--- src/GuessPhrasePanel.java
+++ src/GuessPhrasePanel.java
@@ -6,19 +6,26 @@ public class GuessPhrasePanel extends JPanel {
 	public GuessPhrasePanel(String guessPhrase){
 		this.guessPhrase=guessPhrase;
 	}
+	/*sets a new phrase to this GuessPhrase object,
+	gets rid of old phrase parts and creates new ones
+	*/
 	public void setPhase(String phrase){
 		this.guessPhrase=phrase;
 		
 	}
+	//returns whether or not a specified letter is in the phrase
 	public boolean hasLetter (char c){
 		return false;
 	}
+	//reveals or shows a specified letter
 	public void revealLetter(char c){
 		
 	}
+	//returns whether or not the full phrase has been revealed
 	public boolean isFullPhraseRevealed(){
 		return false;
 	}
+	//reveals the full phrase
 	public String revealFullPhrase(){
 		return guessPhrase;
 	}
