9baace302f8a59c42593aa282423578aab892da2
Takumi Hanai <Takumi Hanai>
2016-06-14 00:25:51 +0900

    Person interface

---
+++ src/Person.java
@@ -0,0 +1,78 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Person extends JPanel {
+	
+	private int numleft=6;
+	
+	
+	public void paintComponent(Graphics page) {
+		
+		int x = 400;
+		int y = 150;
+		
+		super.paintComponent(page);
+		
+		if (numleft == 5){
+			page.setColor(Color.BLUE);
+			page.fillOval(x, y, 180, 180);
+		}
+		
+		if (numleft == 4){
+			page.setColor(Color.yellow);
+			page.fillRect(x, y+180, 190, 250);
+		}
+		
+		if (numleft == 3){
+			page.setColor(Color.green);
+			page.fillRect(x-200, y+185, 200, 90);
+		}
+		
+		if (numleft == 2){
+			page.setColor(Color.red);
+			page.fillRect(x+190, y+185, 200, 90);
+		}
+		
+		if (numleft == 1){
+			page.setColor(Color.cyan);
+			page.fillRect(x, y+430, 85, 250);
+		}
+		
+		if (numleft == 0){
+			page.setColor(Color.magenta);
+			page.fillRect(x+105, y+430, 85, 250);
+		}
+	}
+		
+	public int getNumLeft(){
+		numleft -= 1;
+		return numleft;
+	}
+	
+	public void reset(){
+		
+	}
+	
+	public void shoeNext(){
+		
+	}
+	
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Person");
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			
+		JPanel panel = new Person();
+		panel.setPreferredSize(new Dimension(1000,1000));
+		
+		frame.getContentPane().add(panel);
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+}
