0b02dcc625a752018d90ef1004f325bbdc06df5e
brittany <bgrabowski@drew.edu>
2016-06-13 11:54:14 -0400

    AlphabetPanel outline

--- src/AlphabetPanel.java
+++ src/AlphabetPanel.java
@@ -1,7 +1,44 @@
-
+import java.awt.Color;
 
 public class AlphabetPanel {
 
+	public boolean vowel;
+	public boolean beenSeen;
+	public Color letterColor;
+	
+	
+
+		
+	public AlphabetPanel(){
+		
+	}		
+
+	public void reset(){
+
+	}
+					
+	public void setLetterColor(){
+	
+	}
+
+		
+	public Color getLetterColor(){
+			
+			return null;
+		}
+		
+	public boolean hasLetterBeenSeen(){
+		
+			return beenSeen;
+		}
+			
+	public boolean isVowel(){
+			
+			return vowel;
+			
+		}
+
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
