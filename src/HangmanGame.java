050689ae6e1981ee2ee2b09cd4b8fff09b1d31a6
jegash <jmuneeswaran@drew.edu>
2016-06-14 15:51:12 -0400

    Added comments on main

--- src/HangmanGame.java
+++ src/HangmanGame.java
@@ -16,6 +16,7 @@ public class HangmanGame extends JFrame {
 	private AlphabetPanel ap;
 	private RandomString rs;
 	private GuessPhrasePanel gp;
+	private String word;
 
 	public HangmanGame() {
 		super("Hangman Game");
@@ -42,7 +43,7 @@ public class HangmanGame extends JFrame {
 
 		super.getContentPane().add(p);
 		// super.getContentPane().add(ap);
-		//super.getContentPane().add(rs);
+		// super.getContentPane().add(rs);
 		super.getContentPane().add(gp);
 		super.getContentPane().add(t);
 		super.pack();
@@ -54,31 +55,46 @@ public class HangmanGame extends JFrame {
 		if (c >= 'A' && c <= 'Z') {
 			if (gp.hasLetter(c) == true) {
 				gp.revealLetter(c);
-				//ap.setLetterColor(Color.GREEN);
+				// ap.setLetterColor(Color.GREEN);
 				if (gp.isFullPhraseRevealed()) {
 					wantToRestart();
 				}
 			} else if (p.getNumLeft() > 0) {
-				//if (!ap.hadLetterBeenSeen(c)) {
-					p.showNext();
-					//ap.setLetterColor(Color.RED);
-					if (p.getNumLeft() == 0) {
-						wantToRestart();
-					}
+				// if (!ap.hadLetterBeenSeen(c)) {
+				p.showNext();
+				// ap.setLetterColor(Color.RED);
+				if (p.getNumLeft() == 0) {
+					wantToRestart();
 				}
-			}// else {
-				//wantToRestart();
-		}
-//		} else if (KeyEvent.VK_ENTER == c) {
-//			gp.revealFullPhrase();
-//		} else if (KeyEvent.VK_SPACE == c) {
-//			reset();
-//		}
-//	}
+			}
+		} // else {
+			// wantToRestart();
+	}
+	// } else if (KeyEvent.VK_ENTER == c) {
+	// gp.revealFullPhrase();
+	// } else if (KeyEvent.VK_SPACE == c) {
+	// reset();
+	// }
+	// }
 
 	private void wantToRestart() {
 
 	}
+
+	/**
+	 * This method can be called to test whether the user has guessed all the
+	 * letters in the current secret word. That would mean the user has won the
+	 * game.
+	 */
+//	private boolean wordIsComplete() {
+//		for (int i = 0; i < word.length(); i++) {
+//			char c = word.charAt(i);
+//			if (setPhrase.indexOf(c) == -1) {
+//				return false;
+//			}
+//		}
+//		return true;
+//	}
 
 	public void reset() {
 		p.reset();
@@ -86,14 +102,27 @@ public class HangmanGame extends JFrame {
 		ap.reset();
 	}
 
+	/**
+	 * This main program make it possible to run this class as an application.
+	 * The main routine creates a frame,sets it to contain panel of type
+	 * Hangman, and shows the frame in the center of the screen.
+	 **/
 	public static void main(String[] args) {
 		new HangmanGame();
-		HangmanGame frame = new HangmanGame();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JFrame frame = new JFrame("Hangman");// The frame, with "Hangman" in the
+												// title bar.
+		HangmanGame frame1 = new HangmanGame();// the main frame for the window.
+		frame.getContentPane();
+		frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // End of the
+																// program if
+																// the user
+																// closes the
+																// frame.
 		System.out.println("Click Yes to Restart");
 
-		frame.pack();
-		frame.setVisible(true);
+		frame1.pack(); // Sets the size of the frame based on the preferred
+						// sizes of what it contains.
+		frame1.setVisible(true); // Make the frame visible on the screen.
 	}
 
 }
