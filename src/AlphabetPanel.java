86ba8aef254b6f9c7f7c2fffe2d43d3d42efc474
brittany <bgrabowski@drew.edu>
2016-06-14 12:17:45 -0400

    displays AlphabetPanel

--- src/AlphabetPanel.java
+++ src/AlphabetPanel.java
@@ -1,47 +1,108 @@
-import java.awt.Color;
+import javax.swing.* ;
+import java.awt.* ;
+import java.awt.event.* ;
+import java.util.*;
 
-public class AlphabetPanel {
 
-	public boolean vowel;
+
+public class AlphabetPanel extends JPanel {
+	
+	
+	
 	public boolean beenSeen;
-	public Color letterColor;
+	public boolean vowel;
+	public Color LetterColor;
+	
+	public static char [] alpha  = {'A','B','C','D','E','F','G','H',
+	'I','J','K','L','M','N','O','P','Q','R','S',
+	'T','U','V','W','X','Y','Z'};
 	
 	
-
+	public static char [] vowels  = {'A','E','I','O','U'};
+	public String Letters, letter, letter1;
+	public int num;
+	public int x;
+	public char c;
+	
 		
 	public AlphabetPanel(){
 		
-	}		
-
-	public void reset(){
-
-	}
-					
-	public void setLetterColor(){
-	
-	}
 
 		
-	public Color getLetterColor(){
-			
-			return null;
+		for( c = 'A'; c <= 'Z'; c++){
+			letter = Character.toString(c);
+			Text c = new Text(letter);
+			c.showText();	
+			c.hideUnderline();
+			this.add(c);
 		}
 		
-	public boolean hasLetterBeenSeen(){
+		
+		
+	}
+		
+		
+
+		
+		
+		public void reset(){
+			Component c = this.getComponent(3);
+			c.setBackground(Color.RED);
+			repaint();
+			}
+		
+		
+				
+		public void setLetterColor(String Letters){
+			int i;
+			LetterColor = Color.RED;
+			for(i=0; 1 < vowels.length; i++){
+					if(Letters.contains(String.valueOf(vowels[i]))) {
+						LetterColor = Color.GREEN;
+					}
+			}
+			num = (Letters.charAt(0) - 'A');
+			x = num;
+			repaint();
+		}
+
+		
+		
+		public Color getLetterColor(){
+			
+			return LetterColor;
+		}
+		
+		public boolean hasLetterBeenSeen(){
 		
 			return beenSeen;
 		}
 			
-	public boolean isVowel(){
+		public boolean isVowel(){
 			
 			return vowel;
 			
 		}
 
+
 	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		
+		AlphabetPanel AP = new AlphabetPanel();
+		
+		
+		
+		JFrame frame = new JFrame("ALPHABET PANEL");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//JPanel AlphabetPanel = new JPanel();				
+		AP.setPreferredSize (new Dimension (700,50)) ;
+		
+		frame.getContentPane().add(AlphabetPanel);
+		frame.pack();
+		frame.setVisible(true);
+		
 	}
-
 }
